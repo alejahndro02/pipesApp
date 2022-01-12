57c6e2aad05d3e948112720c0a0943e6a2968515
alejahndro02 <alejahndro02@gmail.com>
2022-01-12 01:07:10 -0600

    se agrego el identificador de item de timpo menuItem y se inicializa en el cilco de vide ngOnInit con valores que tendra el menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  items: MenuItem[]= [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items:[
+          {
+            label: 'Texto y Fechas',
+            icon: 'pi pi-align-left',
+            routerLink:'/'
+          },
+          {
+            label: 'Numeros',
+            icon: 'pi pi-dollar',
+            routerLink:'numeros'
+          },
+          {
+            label: 'No Comunes',
+            icon: 'pi pi-slack',
+            routerLink:'no-comunes'
+          },
+          {
+            label: 'Ordenar',
+            icon: 'pi pi-star',
+            routerLink:'ordenar'
+          }
+        ]
+      },
+      {
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-cog',
+      }
+  ];
   }
 
 }
