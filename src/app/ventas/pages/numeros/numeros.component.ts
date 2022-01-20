13d87ba1940c427c4c4729905f1222b138631b1c
alejahndro02 <alejahndro02@gmail.com>
2022-01-20 00:33:10 -0600

    Se declararon do prpiedades ventasNetas y porcentaje

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
   styleUrls: ['./numeros.component.css']
 })
-export class NumerosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NumerosComponent {
+  vantasNetas: number = 2993881662.29298166255;
+  porcentaje: number = 0.48;
+  
 }
