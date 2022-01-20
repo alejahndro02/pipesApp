b730b0fb4c3922ad0f561b95694c586932b516a0
alejahndro02 <alejahndro02@gmail.com>
2022-01-19 18:59:32 -0600

    Se declararon las propiedades nombreLower, nombreUpper y nombreCompleto

--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
   styleUrls: ['./basicos.component.css']
 })
-export class BasicosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class BasicosComponent {
+  nombreLower:string = 'Alejahndro'
+  nombreUpper:string = 'ALEJAHNDRO '
+  nombreCompleto:string = 'AlEjaHndrO HdZ'
 
 }
