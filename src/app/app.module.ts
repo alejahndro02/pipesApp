7fede1e7525889e69cd18e003290c4496ef4b5c0
alejahndro02 <alejahndro02@gmail.com>
2022-01-19 23:42:20 -0600

    Se cambio el idioma pro defecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
+
+import spanishLanguajeMx from '@angular/common/locales/es-MX'
+import { registerLocaleData } from '@angular/common';
+
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from '@shared/shared.module';
 import { VentasModule } from '@ventas/ventas.module';
+
+
+registerLocaleData(spanishLanguajeMx);
 
 @NgModule({
   declarations: [
@@ -17,7 +25,9 @@ import { VentasModule } from '@ventas/ventas.module';
     SharedModule,
     VentasModule
   ],
-  providers: [],
+  providers: [
+    {provide:LOCALE_ID, useValue:'es-MX'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
