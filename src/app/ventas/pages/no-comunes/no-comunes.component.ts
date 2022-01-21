67195348379fab7efbd62807f3e90f10092da724
alejahndro02 <alejahndro02@gmail.com>
2022-01-21 17:39:24 -0600

    Se declararon las propiedades que usaran los pipes i18nselect y i18nprural

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./no-comunes.component.css']
 })
 export class NoComunesComponent implements OnInit {
-
+  //i18nselect
+ nombre:string = 'Quetzalli'
+ genero: string ='femenino'
+ mapGenero:any= {'femenino':'invitarla', 'masculino':'invitarlo'}
+ //i18nPlural
+ clientes:string[]= ['Quetzalli', 'Fernanda', 'Maria', 'Rodrigo', 'Jose']
+ mapClientes={
+   '=0':'no hay clientes',
+   '=1':  'Tenemos un cliente esperando',
+   '=5': 'tenemos 5 clientes esperando',
+   'other': 'tenemos # clientes esperando'
+ }
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
