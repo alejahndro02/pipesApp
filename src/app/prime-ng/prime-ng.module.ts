327b41809933f3ab0aef6e758687226bd0cc86b2
alejahndro02 <alejahndro02@gmail.com>
2022-01-21 17:42:14 -0600

    se importo el modulo ButtonModule proveniente de primeNg

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 
+
 @NgModule({
   declarations: [],
   exports:[
+    ButtonModule,
     CardModule,
     FieldsetModule,
     MenubarModule,
