4e0352a3cc5f68b733a36a19eaddffd3996cbdfe
alejahndro02 <alejahndro02@gmail.com>
2022-01-20 23:04:26 -0600

    se importo el componente de fieldset desde primeng

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [],
   exports:[
+    CardModule,
+    FieldsetModule,
     MenubarModule,
-    CardModule
   ],
   imports: [
     CommonModule
