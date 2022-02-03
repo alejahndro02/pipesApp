86c2406289964a7372fa2d34ebccacce1b40ee8e
alejahndro02 <alejahndro02@gmail.com>
2022-02-03 14:05:04 -0600

    Se creo el arreglo de objetos para la propiedad de Heroes

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '@ventas/interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
   styleUrls: ['./ordenar.component.css']
 })
-export class OrdenarComponent implements OnInit {
-capitalizarPalabra:boolean= false
-  constructor() { }
-
-  ngOnInit(): void {
+export class OrdenarComponent {
+capitalizarPalabra:boolean= false;
+heroes: Heroe [] =[
+  {
+    nombre: 'Superman',
+    vuela:  true,
+    color:  Color.azul
+  },
+  {
+    nombre: 'Batman',
+    vuela:  false,
+    color:  Color.negro
+  },
+  {
+    nombre: 'Acuaman',
+    vuela:  false,
+    color:  Color.verde
+  },
+  {
+    nombre: 'Goku',
+    vuela:  true,
+    color:  Color.naranja
   }
+]
+
   capitalizar(){
     this.capitalizarPalabra = !this.capitalizarPalabra
   }
+
 }
